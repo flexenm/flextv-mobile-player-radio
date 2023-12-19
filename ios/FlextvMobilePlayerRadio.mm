aea7932328aa108588aec302920f12e6fc00b407
yoo4497 <yoo4497@flexenm.com>
2023-12-19 14:32:56 +0900

    ios 브릿지 추가

--- ios/FlextvMobilePlayerRadio.mm
+++ ios/FlextvMobilePlayerRadio.mm
@@ -2,11 +2,17 @@
 
 @interface RCT_EXTERN_MODULE(FlextvMobilePlayerRadio, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+// RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+//                  withResolver:(RCTPromiseResolveBlock)resolve
+//                  withRejecter:(RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(updatePlayback:(NSDictionary *) originalDetails)
+RCT_EXPORT_METHOD(setNowPlaying:(NSDictionary *) details)
+RCT_EXPORT_METHOD(resetNowPlaying)
+RCT_EXPORT_METHOD(enableControl:(NSString *) controlName enabled:(BOOL) enabled options:(NSDictionary *)options)
+RCT_EXPORT_METHOD(enableBackgroundMode:(BOOL) enabled)
+RCT_EXPORT_METHOD(stopControl)
+RCT_EXPORT_METHOD(observeAudioInterruptions:(BOOL) observe)
 
 + (BOOL)requiresMainQueueSetup
 {
--- ios/FlextvMobilePlayerRadio.swift
+++ ios/FlextvMobilePlayerRadio.swift
@@ -1,8 +1,23 @@
 @objc(FlextvMobilePlayerRadio)
 class FlextvMobilePlayerRadio: NSObject {
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
+  // @objc(multiply:withB:withResolver:withRejecter:)
+  // func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+  //   resolve(a*b)
+  // }
+
+  private let MEDIA_STATE_PLAYING: String = "STATE_PLAYING"
+  private let MEDIA_STATE_PAUSED: String = "STATE_PAUSED"
+  private let MEDIA_STATE_STOPPED: String = "STATE_STOPPED"
+  private let MEDIA_STATE_ERROR: String = "STATE_ERROR"
+  private let MEDIA_STATE_BUFFERING: String = "STATE_BUFFERING"
+  private let MEDIA_STATE_RATING_PERCENTAGE: String = "STATE_RATING_PERCENTAGE"
+  private let MEDIA_SPEED: String = "speed"
+  private let MEDIA_STATE: String = "state"
+  private let MEDIA_DICT: Dictionary<String, Any> = [:]
+
+
+  @objc func updatePlayback(originalDetails: NSDictionary) {
+    
   }
 }
