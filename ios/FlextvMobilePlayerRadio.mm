3f0a34d7c64e1c75cf6e057b8dab06ad309db064
yoo4497 <yoo4497@flexenm.com>
2023-12-19 16:51:03 +0900

    ios 브릿지 연동

--- ios/FlextvMobilePlayerRadio.mm
+++ ios/FlextvMobilePlayerRadio.mm
@@ -7,12 +7,12 @@
 //                  withRejecter:(RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(updatePlayback:(NSDictionary *) originalDetails)
-RCT_EXPORT_METHOD(setNowPlaying:(NSDictionary *) details)
-RCT_EXPORT_METHOD(resetNowPlaying)
-RCT_EXPORT_METHOD(enableControl:(NSString *) controlName enabled:(BOOL) enabled options:(NSDictionary *)options)
-RCT_EXPORT_METHOD(enableBackgroundMode:(BOOL) enabled)
-RCT_EXPORT_METHOD(stopControl)
-RCT_EXPORT_METHOD(observeAudioInterruptions:(BOOL) observe)
+RCT_EXTERN_METHOD(setNowPlaying:(NSDictionary *) details)
+RCT_EXTERN_METHOD(resetNowPlaying)
+RCT_EXTERN_METHOD(enableControl:(NSString *) controlName enabled:(BOOL) enabled options:(NSDictionary *)options)
+RCT_EXTERN_METHOD(enableBackgroundMode:(BOOL) enabled)
+RCT_EXTERN_METHOD(stopControl)
+RCT_EXTERN_METHOD(observeAudioInterruptions:(BOOL) observe)
 
 + (BOOL)requiresMainQueueSetup
 {
--- ios/FlextvMobilePlayerRadio.swift
+++ ios/FlextvMobilePlayerRadio.swift
@@ -6,18 +6,51 @@ class FlextvMobilePlayerRadio: NSObject {
   //   resolve(a*b)
   // }
 
-  private let MEDIA_STATE_PLAYING: String = "STATE_PLAYING"
-  private let MEDIA_STATE_PAUSED: String = "STATE_PAUSED"
-  private let MEDIA_STATE_STOPPED: String = "STATE_STOPPED"
-  private let MEDIA_STATE_ERROR: String = "STATE_ERROR"
-  private let MEDIA_STATE_BUFFERING: String = "STATE_BUFFERING"
-  private let MEDIA_STATE_RATING_PERCENTAGE: String = "STATE_RATING_PERCENTAGE"
-  private let MEDIA_SPEED: String = "speed"
-  private let MEDIA_STATE: String = "state"
-  private let MEDIA_DICT: Dictionary<String, Any> = [:]
+    private let MEDIA_STATE_PLAYING: String = "STATE_PLAYING"
+    private let MEDIA_STATE_PAUSED: String = "STATE_PAUSED"
+    private let MEDIA_STATE_STOPPED: String = "STATE_STOPPED"
+    private let MEDIA_STATE_ERROR: String = "STATE_ERROR"
+    private let MEDIA_STATE_BUFFERING: String = "STATE_BUFFERING"
+    private let MEDIA_STATE_RATING_PERCENTAGE: String = "STATE_RATING_PERCENTAGE"
+    private let MEDIA_SPEED: String = "speed"
+    private let MEDIA_STATE: String = "state"
+    private let MEDIA_DICT: Dictionary<String, Any> = [:]
 
 
-  @objc func updatePlayback(originalDetails: NSDictionary) {
+    @objc func updatePlayback(originalDetails: NSDictionary) {
     
-  }
+    }
+
+    @objc func setNowPlaying(details: NSDictionary) {
+//        MPNowPlayingInfoCenter *center = [MPNowPlayingInfoCenter defaultCenter];
+//        NSMutableDictionary *mediaDict = [NSMutableDictionary dictionary];
+//
+//
+//        center.nowPlayingInfo = [self update:mediaDict with:details andSetDefaults:true];
+//
+//        NSString *artworkUrl = [self getArtworkUrl:[details objectForKey:@"artwork"]];
+//        [self updateArtworkIfNeeded:artworkUrl];
+        let center: MPNowPlayingInfoCenter = MPNowPlayingInfoCenter.default()
+        print("center : \(center)")
+    }
+    
+    @objc func resetNowPlaying() {
+        print("resetNowPlaying!!!")
+    }
+    
+    @objc func enableControl(controlName: NSString, enabled: Bool, options: NSDictionary) {
+        
+    }
+    
+    @objc func enableBackgroundMode(enabled: Bool) {
+        
+    }
+    
+    @objc func stopControl() {
+        
+    }
+    
+    @objc func observeAudioInterruptions(observe: Bool) {
+        
+    }
 }
